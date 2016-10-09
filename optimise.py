3ae2d5eebf94d43e2ad725eba7306b25c393acd9
Ghostkeeper <rubend@tutanota.com>
2016-10-10 01:16:11 +0200

    Implement get_profiles
    This implementation hasn't been tested yet.

--- optimise.py
+++ optimise.py
@@ -17,7 +17,7 @@ import os #To get the current working directory as defaults for input and output
 
 #Global configuration stuff.
 logging.basicConfig(level=logging.DEBUG)
-Profile = collections.namedtuple("Profile", "filepath settings subprofiles")
+Profile = collections.namedtuple("Profile", "filepath settings subprofiles") #Filepath is a path string. Settings is a dictionary of settings. Subprofiles is a set of other Profile instances.
 
 def optimise(input_dir, output_dir):
 	"""
@@ -34,6 +34,8 @@ def optimise(input_dir, output_dir):
 	remove_redundancies(profile_root)
 	write_profiles(output_dir, profile_root)
 
+#################################MAIN STAGES####################################
+
 def get_profiles(input_dir):
 	"""
 	Gets the profile structure described in the input directory.
@@ -44,7 +46,24 @@ def get_profiles(input_dir):
 	:return: The root profile of the profile structure in the input directory.
 	"""
 	logging.info("Reading profiles.")
-	raise Exception("Not implemented yet.")
+	for directory, subdirectories, files in os.walk(input_dir):
+		if subdirectories: #Not a leaf node. Look for a file that is named like the folder.
+			this_directory = os.path.dirname(directory)
+			for file in files:
+				if file.split(".")[0] == this_directory: #Named similarly.
+					directory_node = parse(file)
+					flatten(directory_node)
+					break
+			else: #There was no common file for this directory.
+				directory_node = Profile(
+					filepath=os.path.join(directory, this_directory + ".inst.cfg"),
+					settings={},
+					subprofiles=set()
+				)
+			yield directory_node
+		else: #Leaf node. Find all the rest of the profiles.
+			for file in files:
+				yield flatten(parse(file))
 
 def bubble_common_values(profile_root):
 	"""
@@ -84,6 +103,30 @@ def write_profiles(output_dir, profile_root):
 	subprofiles.
 	"""
 	logging.info("Writing optimised profiles.")
+	raise Exception("Not implemented yet.")
+
+#################################SUBROUTINES####################################
+
+def flatten(profile):
+	"""
+	Flattens a profile, instantiating all settings that it inherits from its
+	parents.
+
+	Its parent is obtained from the profile's filepath. Its parent must already
+	be flattened when calling this function.
+	:param profile: The profile to flatten.
+	:return: A flattened profile.
+	"""
+	raise Exception("Not implemented yet.")
+
+def parse(file):
+	"""
+	Parses one file, creating a Profile instance with all settings from the
+	file.
+	:param file: The file path of the file to parse.
+	:return: A Profile instance, instantiated with all the settings from the
+	file.
+	"""
 	raise Exception("Not implemented yet.")
 
 if __name__ == "__main__":

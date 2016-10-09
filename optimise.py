205104f6b6ab64d6a9511ee0ed0655271676f716
Ghostkeeper <rubend@tutanota.com>
2016-10-10 00:06:27 +0200

    Implement parsing input parameters
    Using the nice ArgumentParser library for this, which changed some of my existing boilerplate already.

--- optimise.py
+++ optimise.py
@@ -7,24 +7,20 @@
 """
 Optimises a set of profiles for Cura.
 
-For usage instructions, run the script without parameters.
+For usage instructions, run the script with the "--help" parameter.
 """
 
-def get_input_from_parameters():
-	raise Exception("Not implemented yet.")
+import argparse #To parse the input and output directories.
+import os #To get the current working directory as defaults for input and output.
 
-def get_output_from_parameters():
-	raise Exception("Not implemented yet.")
-
-def optimise():
-	raise Exception("Not implemented yet.")
-
-def show_instructions():
+def optimise(input_dir, output_dir):
 	raise Exception("Not implemented yet.")
 
 if __name__ == "__main__":
-	input_dir = get_input_from_parameters()
-	output_dir = get_output_from_parameters()
-	if input_dir == output_dir:
-		show_instructions()
-	optimise(input_dir, output_dir)
+	argument_parser = argparse.ArgumentParser(description="Optimise a set of profiles for Cura.")
+	argument_parser.add_argument("-i", dest="input_dir", help="Root directory of input profile structure.", default=os.getcwd())
+	argument_parser.add_argument("-o", dest="output_dir", help="Root directory of output profile structure.", default=os.getcwd())
+	arguments = argument_parser.parse_args()
+	if arguments.input_dir == arguments.output_dir:
+		raise Exception("Input and output directories may not be the same (both were '{dir}').".format(dir=arguments.input_dir))
+	optimise(arguments.input_dir, arguments.output_dir)

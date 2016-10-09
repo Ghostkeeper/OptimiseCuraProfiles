b23ebaae2cc737d2b6de0bca7d00632e88da58fb
Ghostkeeper <rubend@tutanota.com>
2016-10-10 00:57:11 +0200

    Implement algorithm skeleton
    These are the basic steps of the algorithm. None of them are implemented yet. I'll do that now, one by one.

--- optimise.py
+++ optimise.py
@@ -11,9 +11,79 @@ For usage instructions, run the script with the "--help" parameter.
 """
 
 import argparse #To parse the input and output directories.
-import os #To get the current working directory as defaults for input and output.
+import collections #For namedtuple.
+import logging
+import os #To get the current working directory as defaults for input and output, and for file path operations.
+
+#Global configuration stuff.
+logging.basicConfig(level=logging.DEBUG)
+Profile = collections.namedtuple("Profile", "filepath settings subprofiles")
 
 def optimise(input_dir, output_dir):
+	"""
+	Performs the optimisation.
+
+	Files are taken from the input directory in its appropriate structure,
+	flattened, the common denominator is taken, and everything is then written
+	to the output directories.
+	:param input_dir: The root directory of the input profile structure.
+	:param output_dir: The root directory of the output profile structure.
+	"""
+	profile_root = get_profiles(input_dir)
+	bubble_common_values(profile_root)
+	remove_redundancies(profile_root)
+	write_profiles(output_dir, profile_root)
+
+def get_profiles(input_dir):
+	"""
+	Gets the profile structure described in the input directory.
+
+	Each profile is completely flat, meaning that its settings contain every key
+	known in the input directory.
+	:param input_dir: The root of the input directory structure.
+	:return: The root profile of the profile structure in the input directory.
+	"""
+	logging.info("Reading profiles.")
+	raise Exception("Not implemented yet.")
+
+def bubble_common_values(profile_root):
+	"""
+	Finds the common denominator of the profiles in each subgroup, and bubbles
+	them up.
+
+	This makes sure that as many settings as possible have the same value as the
+	setting in the parent profile. That then results in the maximum amount of
+	redundancies to be removed in the ``remove_redundancies`` function.
+
+	The provided root profile is modified to this end.
+	:param profile_root: The root profile, containing all profiles as
+	subprofiles.
+	"""
+	logging.info("Finding common denominators.")
+	raise Exception("Not implemented yet.")
+
+def remove_redundancies(profile_root):
+	"""
+	Removes the settings in each profile that have the same value as its parent.
+
+	The provided root profile is modified to this end.
+	:param profile_root: The root profile, containing all profiles as
+	subprofiles.
+	"""
+	logging.info("Removing redundancies.")
+	raise Exception("Not implemented yet.")
+
+def write_profiles(output_dir, profile_root):
+	"""
+	Writes a profile structure to file.
+
+	All profiles are written as the CFG file format. No other file format has
+	yet been implemented due to time constraints.
+	:param output_dir: The root directory to write the file structure to.
+	:param profile_root: The root profile, containing all profiles as
+	subprofiles.
+	"""
+	logging.info("Writing optimised profiles.")
 	raise Exception("Not implemented yet.")
 
 if __name__ == "__main__":

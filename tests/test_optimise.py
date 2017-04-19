f3b037a9ff09c9ce1ad74e82671c2d8e66129378
Ghostkeeper <rubend@tutanota.com>
2017-04-19 17:01:47 +0200

    Move test data in subdirectory
    This way we can have multiple folders of test data in there. I'm planning to parametrise some of those tests.
    
    Contributes to issue CURA-3650.

--- tests/test_optimise.py
+++ tests/test_optimise.py
@@ -23,26 +23,29 @@ class TestOptimise(unittest.TestCase):
 		"""
 		Tests whether the file path is stored correctly in the profiles.
 		"""
-		profile = optimise.get_profiles(self.data_directory)
-		assert profile.filepath == os.path.join(self.data_directory, "test_data.inst.cfg")
+		input_directory = os.path.join(self.data_directory, "simple_tree")
+		profile = optimise.get_profiles(input_directory)
+		assert profile.filepath == os.path.join(input_directory, "simple_tree.inst.cfg")
 
 	def test_get_profiles_children(self):
 		"""
 		Tests whether the children are properly found when loading profiles.
 		"""
-		profile = optimise.get_profiles(self.data_directory)
+		input_directory = os.path.join(self.data_directory, "simple_tree")
+		profile = optimise.get_profiles(input_directory)
 		assert len(profile.subprofiles) == 1 #One child called 'subdirectory'.
-		assert profile.subprofiles[0].filepath == os.path.join(self.data_directory, "subdirectory", "subdirectory.inst.cfg")
+		assert profile.subprofiles[0].filepath == os.path.join(input_directory, "subdirectory", "subdirectory.inst.cfg")
 
 	def test_get_profiles_grandchildren(self):
 		"""
 		Tests whether the grandchildren are properly found when loading
 		profiles.
 		"""
-		profile = optimise.get_profiles(self.data_directory)
+		input_directory = os.path.join(self.data_directory, "simple_tree")
+		profile = optimise.get_profiles(input_directory)
 		assert len(profile.subprofiles[0].subprofiles) == 2
-		assert profile.subprofiles[0].subprofiles[0].filepath == os.path.join(self.data_directory, "subdirectory", "leaf1.inst.cfg")
-		assert profile.subprofiles[0].subprofiles[1].filepath == os.path.join(self.data_directory, "subdirectory", "leaf2.inst.cfg") #Should also be sorted!
+		assert profile.subprofiles[0].subprofiles[0].filepath == os.path.join(input_directory, "subdirectory", "leaf1.inst.cfg")
+		assert profile.subprofiles[0].subprofiles[1].filepath == os.path.join(input_directory, "subdirectory", "leaf2.inst.cfg") #Should also be sorted!
 
 	def test_get_profiles_weight(self):
 		"""
@@ -51,7 +54,8 @@ class TestOptimise(unittest.TestCase):
 		The weight of a profile should indicate the number of profiles it
 		encompasses.
 		"""
-		profile = optimise.get_profiles(self.data_directory)
+		input_directory = os.path.join(self.data_directory, "simple_tree")
+		profile = optimise.get_profiles(input_directory)
 		assert profile.weight == 4 #test_data, subdirectory, leaf1 and leaf2.
 		assert profile.subprofiles[0].weight == 3 #subdirectory, leaf1 and leaf2.
 		assert profile.subprofiles[0].subprofiles[0].weight == 1 #Just leaf1.
@@ -61,7 +65,8 @@ class TestOptimise(unittest.TestCase):
 		"""
 		Tests whether loaded profiles have the correct settings.
 		"""
-		profile = optimise.get_profiles(self.data_directory)
+		input_directory = os.path.join(self.data_directory, "simple_tree")
+		profile = optimise.get_profiles(input_directory)
 		assert profile.settings["apples"] == "3"
 		assert profile.settings["oranges"] == "5"
 		assert profile.settings["bananas"] == "-1"
